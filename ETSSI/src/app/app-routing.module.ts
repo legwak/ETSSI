6f353accb4066c964a468b4dd564ba0462e0a92f
Unknown <jordan.lebrun@viacesi.fr>
2017-12-08 10:49:25 +0100

    Auto stash before merge of "master" and "origin/master"

--- ETSSI/src/app/app-routing.module.ts
+++ ETSSI/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 const routes: Routes = [
     {
